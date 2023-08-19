08066485f0703c70d9b91437bd57057dd40113f9
Dustin Brown <dbrown9@gmail.com>
2023-08-19 14:19:59 -0700

    fix issue with adding key to dict

--- app/functions.py
+++ app/functions.py
@@ -1,13 +1,14 @@
 import modal
 
-def download_whisper():
-  # Load the Whisper model
-  import os
-  import whisper
-  print ("Download the Whisper model")
 
-  # Perform download only once and save to Container storage
-  whisper._download(whisper._MODELS["medium"], '/content/podcast/', False)
+def download_whisper():
+    # Load the Whisper model
+    import os
+    import whisper
+    print("Download the Whisper model")
+
+    # Perform download only once and save to Container storage
+    whisper._download(whisper._MODELS["medium"], '/content/podcast/', False)
 
 
 stub = modal.Stub("corise-podcast-project")
@@ -20,84 +21,88 @@ corise_image = modal.Image.debian_slim().pip_install("feedparser",
                                                      "wikipedia",
                                                      "ffmpeg-python").apt_install("ffmpeg").run_function(download_whisper)
 
+
 @stub.function(image=corise_image, gpu="any", timeout=600)
 def get_transcribe_podcast(rss_url, local_path):
-  print ("Starting Podcast Transcription Function")
-  print ("Feed URL: ", rss_url)
-  print ("Local Path:", local_path)
+    print("Starting Podcast Transcription Function")
+    print("Feed URL: ", rss_url)
+    print("Local Path:", local_path)
 
-  # Read from the RSS Feed URL
-  import feedparser
-  intelligence_feed = feedparser.parse(rss_url)
-  podcast_title = intelligence_feed['feed']['title']
-  episode_title = intelligence_feed.entries[0]['title']
-  episode_image = intelligence_feed['feed']['image'].href
-  for item in intelligence_feed.entries[0].links:
-    if (item['type'] == 'audio/mpeg'):
-      episode_url = item.href
-  episode_name = "podcast_episode.mp3"
-  print ("RSS URL read and episode URL: ", episode_url)
+    # Read from the RSS Feed URL
+    import feedparser
+    intelligence_feed = feedparser.parse(rss_url)
+    podcast_title = intelligence_feed['feed']['title']
+    episode_title = intelligence_feed.entries[0]['title']
+    episode_image = intelligence_feed['feed']['image'].href
+    for item in intelligence_feed.entries[0].links:
+        if (item['type'] == 'audio/mpeg'):
+            episode_url = item.href
+    episode_name = "podcast_episode.mp3"
+    print("RSS URL read and episode URL: ", episode_url)
 
-  # Download the podcast episode by parsing the RSS feed
-  from pathlib import Path
-  p = Path(local_path)
-  p.mkdir(exist_ok=True)
+    # Download the podcast episode by parsing the RSS feed
+    from pathlib import Path
+    p = Path(local_path)
+    p.mkdir(exist_ok=True)
 
-  print ("Downloading the podcast episode")
-  import requests
-  with requests.get(episode_url, stream=True) as r:
-    r.raise_for_status()
-    episode_path = p.joinpath(episode_name)
-    with open(episode_path, 'wb') as f:
-      for chunk in r.iter_content(chunk_size=8192):
-        f.write(chunk)
+    print("Downloading the podcast episode")
+    import requests
+    with requests.get(episode_url, stream=True) as r:
+        r.raise_for_status()
+        episode_path = p.joinpath(episode_name)
+        with open(episode_path, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=8192):
+                f.write(chunk)
 
-  print ("Podcast Episode downloaded")
+    print("Podcast Episode downloaded")
 
-  # Load the Whisper model
-  import os
-  import whisper
+    # Load the Whisper model
+    import os
+    import whisper
 
-  # Load model from saved location
-  print ("Load the Whisper model")
-  model = whisper.load_model('medium', device='cuda', download_root='/content/podcast/')
+    # Load model from saved location
+    print("Load the Whisper model")
+    model = whisper.load_model(
+        'medium', device='cuda', download_root='/content/podcast/')
 
-  # Perform the transcription
-  print ("Starting podcast transcription")
-  result = model.transcribe(local_path + episode_name)
+    # Perform the transcription
+    print("Starting podcast transcription")
+    result = model.transcribe(local_path + episode_name)
 
-  # Return the transcribed text
-  print ("Podcast transcription completed, returning results...")
-  output = {}
-  output['podcast_title'] = podcast_title
-  output['episode_title'] = episode_title
-  output['episode_image'] = episode_image
-  output['episode_transcript'] = result['text']
-  return output
+    # Return the transcribed text
+    print("Podcast transcription completed, returning results...")
+    output = {}
+    output['podcast_title'] = podcast_title
+    output['episode_title'] = episode_title
+    output['episode_image'] = episode_image
+    output['episode_transcript'] = result['text']
+    return output
+
 
 @stub.function(image=corise_image, secret=modal.Secret.from_name("my-openai-secret"))
 def get_podcast_summary(podcast_transcript):
-  import openai
-  summary_prompt = """
+    import openai
+    summary_prompt = """
     You are a copywriter working for a podcast company. Your job is to write a short description of a
     podcast episode that will entice people to listen to it. You have been given a transcript of the
     episode below. Keep the description to 280 characters or less.
 
   """
 
-  request = summary_prompt + podcast_transcript
+    request = summary_prompt + podcast_transcript
 
-  chatOutput = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo-16k",
-    messages=[
-      {"role": "system", "content": "You are a helpful assistant."},
-      {"role": "user", "content": request}
-    ]
-  )
+    chatOutput = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo-16k",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": request}
+        ]
+    )
 
-  summary = chatOutput.choices[0].message.content
+    summary = chatOutput.choices[0].message.content
 
-  return summary
+    return summary
+
 
 @stub.function(image=corise_image, secret=modal.Secret.from_name("my-openai-secret"))
 def get_podcast_guest(podcast_transcript):
@@ -152,7 +157,8 @@ def get_podcast_guest(podcast_transcript):
     # The response indicates that the model wants to call the function
     if response_message.get("function_call"):
         function_name = response_message["function_call"]["name"]
-        function_args = json.loads(response_message["function_call"]["arguments"])
+        function_args = json.loads(
+            response_message["function_call"]["arguments"])
         guests = function_args.get("guests")
 
     if function_name != "get_interesting_people":
@@ -161,20 +167,22 @@ def get_podcast_guest(podcast_transcript):
     for guest in guests:
         wiki = None
         try:
-            wiki = wikipedia.page(f"{guest.get('name')} {guest.get('title')} {guest.get('organization')}", auto_suggest=True)
+            wiki = wikipedia.page(
+                f"{guest.get('name')} {guest.get('title')} {guest.get('organization')}", auto_suggest=True)
         except:
             None
 
         if wiki is not None:
-            guest.set("wiki", {
+            guest.update({"wiki": {
                 "title": wiki.title,
                 "url": wiki.url,
                 "summary": wiki.summary,
-            })
+            }})
         else:
             print(f"No wikipedia page found for {guest.get('name')}")
 
     return guests
+
 
 @stub.function(image=corise_image, secret=modal.Secret.from_name("my-openai-secret"))
 def get_podcast_highlights(podcast_transcript):
@@ -206,14 +214,18 @@ def get_podcast_highlights(podcast_transcript):
 
     return podcast_highlights
 
+
 @stub.function(image=corise_image, secret=modal.Secret.from_name("my-openai-secret"), timeout=1200)
 def process_podcast(url, path):
     output = {}
     podcast_details = get_transcribe_podcast.call(url, path)
 
-    podcast_summary = get_podcast_summary.call(podcast_details['episode_transcript'])
-    podcast_guests = get_podcast_guest.call(podcast_details['episode_transcript'])
-    podcast_highlights = get_podcast_highlights.call(podcast_details['episode_transcript'])
+    podcast_summary = get_podcast_summary.call(
+        podcast_details['episode_transcript'])
+    podcast_guests = get_podcast_guest.call(
+        podcast_details['episode_transcript'])
+    podcast_highlights = get_podcast_highlights.call(
+        podcast_details['episode_transcript'])
 
     output['podcast_details'] = podcast_details
     output['podcast_summary'] = podcast_summary
@@ -221,10 +233,14 @@ def process_podcast(url, path):
     output['podcast_highlights'] = podcast_highlights
     return output
 
+
 @stub.local_entrypoint()
 def test_method(url, path):
     output = {}
     podcast_details = get_transcribe_podcast.call(url, path)
-    print ("Podcast Summary: ", get_podcast_summary.call(podcast_details['episode_transcript']))
-    print ("Podcast Guest Information: ", get_podcast_guest.call(podcast_details['episode_transcript']))
-    print ("Podcast Highlights: ", get_podcast_highlights.call(podcast_details['episode_transcript']))
+    print("Podcast Summary: ", get_podcast_summary.call(
+        podcast_details['episode_transcript']))
+    print("Podcast Guest Information: ", get_podcast_guest.call(
+        podcast_details['episode_transcript']))
+    print("Podcast Highlights: ", get_podcast_highlights.call(
+        podcast_details['episode_transcript']))
